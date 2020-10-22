985b179da95f95db158684c4d28e92bf198da25f
Oleksandr Boitsov <kontorab@gmail.com>
2020-10-22 15:09:23 +0300

    feat: add algorithm towel sort

--- src/index.js
+++ src/index.js
@@ -7,8 +7,7 @@ module.exports = function towelSort (matrix) {
  }
     let newArr = [];
     for (let i=0; i < matrix.length; i++) {
-        if (i % 2 !== 0) {
-            matrix[i] = matrix[i].reverse(); 
+        if (i % 2 !== 0) { matrix[i] = matrix[i].reverse(); 
         }
         newArr = newArr.concat(matrix[i])
     } 
